bb3267623985db252fed343e3a7912c2960f17d6
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2019-12-15 13:40:32 +0530

    Added the voice feature

--- app/src/main/java/com/example/attendura/MainActivity.java
+++ app/src/main/java/com/example/attendura/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.attendura;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void voice(View view)
+    {
+        Intent intent =new Intent(this,Activity_voice.class);
+        startActivity(intent);
     }
 }
