445a862c873bf3906c4786ee5c6a9b6c558c7839
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2019-12-30 18:19:05 +0530

    Added Menu

---
+++ app/src/main/java/com/example/attendura/Input_Activity.java
@@ -0,0 +1,15 @@
+package com.example.attendura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Input_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_);
+        getSupportActionBar().setTitle("User Inputs");
+    }
+}
--- app/src/main/java/com/example/attendura/MainActivity.java
+++ app/src/main/java/com/example/attendura/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.attendura;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toolbar;
 
@@ -14,7 +18,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Home");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.home_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Intent intent = new Intent(this, Input_Activity.class);
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public void voice(View view)
     {
         Intent intent =new Intent(this,Activity_voice.class);
